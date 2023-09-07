178dc5e201da8e2d5970333dab934a716ad7f70f
leqanh2302 <leqanh2302@gmail.com>
2023-09-07 18:07:42 +0700

    console

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program 
+{
+    private static void Main(String[] args)
+    {
+        Console.Write("Nhap vao du lieu: ");
+        int a = Console.ReadLine();
+        Console.WriteLine("Du lieu vua nhap vao la: " + str);
+    
+    }
+}
