134914d8bd57a852a1f2f78285d77f35a59a8291
leqanh2302 <leqanh2302@gmail.com>
2023-09-10 21:43:36 +0700

    bienso

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,3 +1,34 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    //khai bao cac bien so
+         // int a = 2;
+        // Console.WriteLine(a);
+        
+       // float a = 2.3;
+       // console.WriteLine(a)
+
+       // bool t = true
+       // bool f = false
+       // Console.WriteLine(e);
+       // Console.WriteLine(f);
+
+       // char ky tu = 'q'
+      // Console.WriteLine(ky tu);
+
+      // string str = "anh"
+      //Console.WriteLine(str);
+
+      int msv = 2021050074;
+      Console.WriteLine("msv=" + msv);
+
+      
+
+
+      
+
+
+
+    }
+}
