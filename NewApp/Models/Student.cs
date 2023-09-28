b867a9f7402219846233c615e4db68093a4e3a47
leqanh2302 <leqanh2302@gmail.com>
2023-09-28 23:51:56 +0700

    Khoi tao Student

--- NewApp/Models/Student.cs
+++ NewApp/Models/Student.cs
@@ -1,26 +1,18 @@
-namespace NewApp.Models
+using NewApp.Models;
+public class Student
 {
-    public class Student
+    private static void Main(string[] args)
     {
-        public int MaSinhVien {get; set;}
-        public string TenSinhVien {get; set;}
-        public int Tuoi {get; set;}
-        public string DiaChi {get; set;}
-        publicvoid EnterData()
-        {
-            System.Console.WriteLine("MaSinhVien = ");
-            MaNhanVien = Convert.ToInt16(Console.ReadLine());
-            System.Console.WriteLine("TenSinhVien = ");
-            TenNhanVien = Console.ReadLine();
-            System.Console.WriteLine("Tuoi");
-            Tuoi = Convert.ToInt16(Console.ReadLine());
-            System.Console.WriteLine("DiaChi");
-            DiaChi = Console.ReadLine();
-        }
-        public void Display()
-        {
-            System.Console.WriteLine("{0} - {1} - {2} tuoi", MaSinhVien, TenSinhVien, Tuoi, DiaChi);
-        }
+        //khoitaodoituong
+        Student std1 = new Student();
+        Student std2 = new Student();
+        //gangiatrithuoctinh
+        std1.FullName = "LQA";
+        std2.Address = "Ha Noi";
+        //goi pt hien thi thong tin
+        std1.Display();
+        std2.Display();
     }
 }
+     
 // Le Quynh Anh_2021050074
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+using NewApp.Model;
+public class Program
+{
+    private static void Main(String[] args)
+    {
+    person ps1 = new person();
+    person ps2 = new person();
+    ps1.FullName = "NVA";
+    ps1.Address = "HN";
+    ps1.Display();
+    ps2.Display();
+    }
+    public void 
+
+
+}
