437cfceee393b2f76dd922d327f9db075a3fc7c7
leqanh2302 <leqanh2302@gmail.com>
2023-10-25 21:53:54 +0700

    luong...

---
+++ Models/BT.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using DemoMVC.Models;
+namespace DemoMVC.Model
+{
+    public class BTluong
+    {
+        public string HoTen {get; set;}
+        public int HSluong {get; set;}
+        public int PhuCap {get; set;}
+        public int Luongcb {get; set;}
+
+    }
+}
+
+//Le Quynh Anh_2021050074
